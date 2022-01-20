a1b4b842fd0b86f960a54c1bf8e190719dce8e50
akemi798bet <akemi798bet@gmail.com>
2022-01-20 11:05:49 +0700

    first commit

---
+++ 8-os-module.js
@@ -0,0 +1,14 @@
+// OS
+const os = require('os')
+
+const user = os.userInfo()
+console.log(user);
+
+console.log(`The System Uptime is: ${os.uptime()} senconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem : os.totalmem(),
+    freemem : os.freemem()
+}
